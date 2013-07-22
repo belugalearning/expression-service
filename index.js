06c15c42c8ec41db46946ca39528b4ff5b277250
Nick Cartwright <nick@aslant.io>
2013-07-22 22:27:08 +0100

    broken commit for sake of submodule fuckaround

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
-window.bl = window.bl || {};
-window.bl.expressionService = {}
-window.top.bl.expressionService = window.bl.expressionService;
+if (false) {
+window.bl = window.bl || {}
+//window.top.bl = window.bl
+
+window.bl.expressionService = (function() {
+  return {
+    evaluateExpression: function(o) {
+      return evaluate($.parseXML(o.expression).childNodes[0], o.symbols)
+    }
+  }
+
+  function evaluate(expression, symbols) {
+    switch (expression.nodeName) {
+      case 'apply':
+        return apply(expression.childNodes, symbols)
+      case 'csymbol':
+        resolveCSymbol(expression, symbols)
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  function apply(nodes, symbols) {
+    var op = nodes[0].nodeName
+    var args = [].slice.call(nodes, 1).map(function(node) {
+      return evaluate(node, symbols)
+    })
+
+    var fn = ({
+      'in': isIn,
+      'notin': notIn
+    })[op]
+
+    return fn.apply(null, args)
+  }
+
+  function isIn(member, set) {
+    console.log('isIn arguments:', arguments)
+    return true
+  }
+
+  function notIn() {
+    return !isIn.apply(null, arguments)
+  }
+
+  function resolveCSymbol(csymbol, symbols) {
+    if (!/^local:\/\//.test(csymbol)) throw new Error('invalid protocol for retrieving csymbol definition')
+    console.log(csymbol.match(/\/([^\/]+)/g))
+  }
+}())
+}
